6005dd95b6022d5c07ab0f9bef841536740ec35a
MR KHAN <shahnawazkhan96850@gmail.com>
2024-11-16 16:53:09 +0530

    Unique Item Set

--- set_1.py
+++ set_1.py
@@ -1,4 +1,10 @@
-user = [10,2,34,2,6,10,"sum"]
+"""user = [10,2,34,2,6,10,"sum"]
 my_set=set(user)
 new_list = list(my_set)
-print("Unique List:",new_list)
+print("Unique List:",new_list)"""
+#***********************************
+#Add items in Tuple
+Tuple1 = (input(),input(),input())
+print(Tuple1)
+
+
