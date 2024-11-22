e2010fe5db26ebd6a8030a69659ef85cdb0c8b7d
MR KHAN <shahnawazkhan96850@gmail.com>
2024-11-22 18:09:52 +0530

    Root Quadratic Equation

--- root_quadratic_equation.py
+++ root_quadratic_equation.py
@@ -1,8 +1,19 @@
 import math
-a1 = int(input("Enter The Value First:"))
-b2 = int(input("Enter The Value Second :"))
-c3 = int(input("Enter The Value Third :"))
-x1 = ((-b2)+(math.sqrt((b2**2)-4*a1*c3)))/(2*a1)
-x2 = ((-b2)-(math.sqrt((b2**2)-4*a1*c3)))/(2*a1)
-print("Value x1 is:",x1)
-print("Value x2 is :",x2)
+a_user = int(input("Enter The First  Value:"))
+b_user = int(input("Enter The Second Value:"))
+c_user = int(input("Enter The Third  Value:"))
+temp = (b_user**2)-(4*a_user*c_user)
+print("The Discriminant is:",temp)
+if temp>0:	
+	x = ((-b_user)+math.sqrt(temp))/(2*a_user)
+	y = ((-b_user)-math.sqrt(temp))/(2*a_user)
+	print("Discriminant value is positive, the quadratic equation has two real and distinct solutions: x = {} ,y = {}".format(int(x),int(y)))
+elif temp==0:
+	x = ((-b_user)+math.sqrt(temp))/(2*a_user)
+	y = ((-b_user)-math.sqrt(temp))/(2*a_user)
+	print("Discriminant value is zero, the quadratic equation has only one solution or two real and equal solutions: x = {} ,y = {}".format(int(x),int(y)))
+elif temp<0:
+	print("The square root of a negative number is not a real number; instead, it results in an imaginary number.")
+	print("Therefore, the quadratic equation has no real solutions, but it has two complex (imaginary) solutions.")
+else:
+	print("Inalid Syntax")
