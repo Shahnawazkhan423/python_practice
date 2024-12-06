ef9e00445baf2b3def235caf63d4795834771aa1
MR KHAN <shahnawazkhan96850@gmail.com>
2024-12-06 19:32:42 +0530

    CalculateLength List

---
+++ List_len.py
@@ -0,0 +1,23 @@
+List1 = [1,"Khan",2.5,5]
+'''for i in range(len(List1)):
+	print(List1[i])'''
+print()
+#******************
+'''for i in List1:
+	print(i)'''
+#Adding an element at max index or at last Step
+#print(List1)
+#List1.append(6)
+#print(List1)
+#Adding Element at Specifice Index
+#List1.insert(-1,"Indore")
+#Replace Any Value
+#List1[1]="Mohit"
+'''List1[3]=78
+print(List1)'''
+#Remove Specifices Value
+List1.remove(2.5)
+print(List1)
+#Del Last Value
+List1.pop()
+print(List1)
