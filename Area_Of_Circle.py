0985f2c436f6a67603593684e0413f5fbc02cd99
MR KHAN <shahnawazkhan96850@gmail.com>
2024-12-11 19:16:31 +0530

    Area Of Circle

---
+++ Area_Of_Circle.py
@@ -0,0 +1,8 @@
+class Circle:
+	def __init__(self):
+		self.radius = int(input("Enter The Radius Value: "))
+	def cal(self):
+		return 3.14*self.r.adius*self.radius
+cir = Circle()
+araCir = cir.cal()
+print("Area Of Circle is {}:{}".format(cir.radius,araCir))
