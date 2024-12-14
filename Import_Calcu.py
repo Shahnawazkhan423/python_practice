d9246e2cf8eff31b3d2fa4b4a7d8fbe9b51c39ab
MR KHAN <shahnawazkhan96850@gmail.com>
2024-12-14 18:04:32 +0530

    Import The Value To Calculate

---
+++ Import_Calcu.py
@@ -0,0 +1,8 @@
+def add(x,y):
+	print("Sum Of Two Number:",x+y)
+def sub(x,y):
+	print("Sub Of Two Number:",x-y)
+def mul(x,y):
+	print("Mul Of Two Number:",x*y)
+def divi(x,y):
+	print("Divi Of Two Number:",x/y)
