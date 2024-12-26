1101a8e9f900f793d143e2e02c60822616e5e331
MR KHAN <shahnawazkhan96850@gmail.com>
2024-12-26 21:53:44 +0530

    This Program counts letters one by One Given And How many  Same Letter shows the Output

--- String_Alpha_Count.py
+++ String_Alpha_Count.py
@@ -1,4 +1,4 @@
-user = "abcdf"
+user = "abbfda"
 string =""
 count=1
 for i in range(1,len(user)):
@@ -6,6 +6,6 @@ for i in range(1,len(user)):
         count+=1
     else:   
         string+=(user[i-1]+str(count))
-        print(user[i-1])
         count=1
+string+=(user[i]+str(count))
 print(string)
